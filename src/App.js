a9b07a939173eb7dc6dbc4f08a8b5687ad38048c
Dustin Myers <dustinmyers@dustins-mbp.lan>
2018-10-07 08:50:44 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase';
+import Form from './components/MessageForm.js';
+import conferenceCenter from './assets/conferenceCenter.jpg'
+import firebaseConfig from './config';
 import './App.css';
-
+firebase.initializeApp(firebaseConfig);
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    }
+  }
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+    });
+  }
+  handleSignIn = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+  handleLogOut = () => {
+    firebase.auth().signOut();
+    this.setState({ user: null });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="app__header" style={{ backgroundImage: `url(${conferenceCenter})` }}>
+          <h1>
+            Myers Conference Chat
+          </h1>
+          { !this.state.user ? (
+            <button
+              className="app__button"
+              onClick={this.handleSignIn}
+            >
+              Sign in
+            </button>
+          ) : (
+            <button
+              className="app__button"
+              onClick={this.handleLogOut}
+            >
+              Logout
+            </button>
+          )}
+        </div>
+        <div className="app__list">
+          <Form user={this.state.user} />
+        </div>
       </div>
     );
   }
 }
-
 export default App;
